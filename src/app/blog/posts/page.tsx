df207bb87c45cb62fe5532bb16706b0da9c00a8f
Adarsh Nanda <adarsh.ch.nanda@gmail.com>
2024-12-03 23:54:33 +0530

    check database connection

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,9 +1,12 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
+import { connectToDB } from "@/app/lib/data";
+export default async function Page() {
+  const client = await connectToDB();
 
-export default function Page() {
   return (
     <>
+    {client&& <>Connected to database</>}
       <h1 className="text-4xl text-purple-800">Posts</h1>
       {posts.map((post) => (
         <Post key={post.id} {...post} />
